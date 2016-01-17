9dae512efe5d5254dc59ff571521d22311eb76d3
David Francos Cuartero <dfrancos@buguroo.com>
2016-01-17 20:27:59 +0100

    The template added literal ' there =P

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Aircrack-ng python 'bindings''
+project = u'Aircrack-ng python \'bindings\''
 copyright = u'2015, David Francos Cuartero'
 
 # The version info for the project you're documenting, acts as replacement
