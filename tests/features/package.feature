dc21f07548a1cb2ab0bb90ac2da19df75c31c396
David Francos <opensource@davidfrancos.net>
2018-01-09 03:24:53 +0100

    Initial commit

---
+++ tests/features/package.feature
@@ -0,0 +1,5 @@
+ Feature: Our software must be easily redistributable
+Scenario: Check that we provide an importable package
+    When: I import the package pyrcrack
+    Then: I see no errors
+
---
+++ tests/features/steps/steps_package.py
@@ -0,0 +1,17 @@
+from behave import when, then
+
+
+@when('I import the package pyrcrack')
+def import_package(context):
+    """Try to import the package. If an exception happened push to context."""
+    context.exceptions = []
+    try:
+        import pyrcrack
+    except ImportError as excp:
+        context.exceptions = [excpt]
+
+
+@then('I see no errors')
+def no_exceptions_in_context(context):
+    """Check that no previous exceptions have been captured."""
+    assert not context.exceptions
