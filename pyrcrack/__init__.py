c935c0da0222dd7b4a1a92fe9b30bdc7e582cfc4
David Francos <opensource@davidfrancos.net>
2020-10-02 01:06:59 +0200

    wip: #16 Aircrack-ng 1.6 and new AirodumpNG API
    This introduces the changes stated in #16.
    A new async iterator for airodump-ng class, uses netxml files, detects
    process finish and issues trying to launch the current command (and
    reports the issues wich didn't before, as stated in #8

--- pyrcrack/__init__.py
+++ pyrcrack/__init__.py
@@ -2,6 +2,7 @@
 
 Aircrack-NG python bindings
 """
+import asyncio
 import subprocess
 
 from .aircrack import AircrackNg  # noqa
@@ -15,4 +16,54 @@ from .airodump import AirodumpNg  # noqa
 
 def check():
     """Check if aircrack-ng is compatible."""
-    assert '1.3' in subprocess.check_output(['aircrack-ng', '-v'])
+    assert '1.6' in subprocess.check_output(['aircrack-ng', '-v'])
+
+
+class Pyrcrack:
+    """High level aircrack-ng interface.
+    
+    Arguments: 
+
+        list_waiting_time: Time (in seconds) to wait for a network scan
+    """
+    def __init__(self, list_waiting_time):
+        """Set up pyrcrack instance."""
+        self.iface = None
+        self.list_waiting_time = list_waiting_time
+
+    @property
+    async def interfaces(self):
+        """List of currently available interfaces as reported by airmon-ng
+
+        This is an awaitable property, use it as in::
+
+            await Pyrcrack().interfaces
+
+        Returns: None
+        """
+        return [a['interface'] for a in await airmon.list_wifis()]
+
+    async def set_interface(self, interface):
+        """Select an interface, set it on monitor mode
+
+        Will set `iface` property.
+        """
+        assert interface in await self.interfaces
+        async with AirmonNg() as airmon:
+            interface = await airmon.set_monitor(interface)
+            self.iface = interface[0]
+
+    @property
+    async def access_points(self) -> list:
+        """Return a list of access points by scanning with airodump-ng
+
+        This is a basic, hihg-level scan, on all channels without extra
+        options, for more custom searches uses directly the AirodumpNg async
+        context manager.
+
+        Returns: List of `AccessPoint` instances
+        """
+        async with AirodumpNg() as pdump:
+            await pdump.run(self.iface, write_interval=1)
+            await asyncio.sleep(self.list_waiting_time)
+            return pdump.sorted_aps()
--- pyrcrack/airodump.py
+++ pyrcrack/airodump.py
@@ -1,16 +1,127 @@
-"""Aidorump."""
+"""Airodump."""
 
+import os
 from glob import glob
 import asyncio
 import csv
 import io
+import xml
 from contextlib import suppress
 from .executor import ExecutorHelper
-from .models import AccessPoint, Client
+
+from async_timeout import timeout
+from rich.table import Table
+import xmltodict
+import dotmap
+
+DICTS = ('WLAN_', 'JAZZTEL_', 'MOVISTAR_')
+
+class Result(list):
+    @property
+    def table(self):
+        """Return a nicely formatted table with results."""
+        table = Table(show_header=True,
+                      header_style='bold magenta',
+                      show_footer=False)
+        table.add_column('Name')
+        table.add_column('Bssid')
+        table.add_column('Power')
+        table.add_column('Captured packets')
+        table.add_column('Score')
+        for wireless in self: 
+            table.add_row(wireless.essid, wireless.bssid, str(wireless.dbm),
+                          str(wireless.packets.total), str(wireless.score))
+        return table
+
+
+
+class Client:
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def bssid(self):
+        return self.data['client-mac']
+
+    @property
+    def packets(self):
+        return self.data.packets.total
+
+    @property
+    def dbm(self):
+        return self.data['snr-info'].last_signal_dbm
+
+
+class AccessPoint:
+    """Represents an access point.
+    
+    Stores internal data in "data" property
+    """
+    def __init__(self, data):
+        """Initialize an access point
+
+        Arguments:
+
+            data: Dot data structure from kismet xml file.
+        """
+        self.data = data
+
+    def __repr__(self):
+        return f"{self.essid} - ({self.bssid})"
+
+    @property
+    def clients(self):
+        """List of connected clients.
+
+        Returns:
+
+            List of Client instance.
+        """
+        if isinstance(self.data['wireless-client'], list):
+            return [Client(d) for d in self.data['wireless-client']]
+        else:
+            return [Client(self.data['wireless-client'])]
+
+    @property
+    def essid(self):
+        """Essid"""
+        return self.data.SSID.essid.get('#text', '')
+
+    @property
+    def bssid(self):
+        """Mac (BSSID)"""
+        return self.data.BSSID
+
+    @property
+    def score(self):
+        """Score, used to sort networks.
+
+        Score will take in account the total packets received, the dbm and if a
+        ssid is susceptible to have dictionaries.
+        """
+        packet_score = int(self.packets.total)
+        dbm_score = -int(self.dbm)
+        dict_score = bool(any(self.essid.startswith(a) for a in DICTS))
+        name_score = -1000 if not self.essid else 0
+        return packet_score + dbm_score + dict_score
+
+    @property
+    def packets(self):
+        """Return list of packets."""
+        return self.data.packets
+
+    @property
+    def dbm(self):
+        """Return dbm info"""
+        return self.data['snr-info'].last_signal_dbm
+
+    def __lt__(self, other):
+        """Compare with score."""
+        return self.score
 
 
 class AirodumpNg(ExecutorHelper):
-    """Airodump-ng 1.3  - (C) 2006-2018 Thomas d'Otreppe
+    """Airodump-ng 1.6  - (C) 2006-2020 Thomas d'Otreppe
        https://www.aircrack-ng.org
 
        usage: airodump-ng <options> <interface>[,<interface>,...]
@@ -19,7 +130,7 @@ class AirodumpNg(ExecutorHelper):
            --ivs                 : Save only captured IVs
            --gpsd                : Use GPSd
            --write <prefix>      : Dump file prefix
-           -w <prefix>           : same as --write
+           -w                    : same as --write
            --beacons             : Record all beacons in dump file
            --update <secs>       : Display update delay in seconds
            --showack             : Prints ack/cts/rts statistics
@@ -28,21 +139,23 @@ class AirodumpNg(ExecutorHelper):
            --berlin <secs>       : Time before removing the AP/client
                                    from the screen when no more packets
                                    are received (Default: 120 seconds)
-           -r <file>             : Read packets from that file
+           -r  <file>            : Read packets from that file
+           -T                    : While reading packets from a file,
+                                   simulate the arrival rate of them
+                                   as if they were "live".
            -x <msecs>            : Active Scanning Simulation
            --manufacturer        : Display manufacturer from IEEE OUI list
            --uptime              : Display AP Uptime from Beacon Timestamp
            --wps                 : Display WPS information (if any)
-           --output-format
-                       <formats> : Output format. Possible values:
-                                   pcap, ivs, csv, gps, kismet, netxml
+           --output-format <formats> : Output format. Possible values:
+                                       pcap, ivs, csv, gps, kismet, netxml,
+                                       logcsv
            --ignore-negative-one : Removes the message that says
                                    fixed channel <interface>: -1
-           --write-interval <seconds> : Output file(s) write
-                                        interval in seconds
+           --write-interval <seconds> : Output file(s) write interval in seconds
            --background <enable> : Override background detection.
-
-       Filter options:
+           -n <int>              : Minimum AP packets recv'd before for
+                                   displaying it
            --encrypt <suite>     : Filter APs by cipher suite
            --netmask <netmask>   : Filter APs by mask
            --bssid <bssid>       : Filter APs by BSSID
@@ -50,9 +163,6 @@ class AirodumpNg(ExecutorHelper):
            --essid-regex <regex> : Filter APs by ESSID using a regular
                                    expression
            -a                    : Filter unassociated clients
-
-       By default, airodump-ng hop on 2.4GHz channels.
-       You can make it capture on other/specific channel(s) by using:
            --ht20                : Set channel to HT20 (802.11n)
            --ht40-               : Set channel to HT40- (802.11n)
            --ht40+               : Set channel to HT40+ (802.11n)
@@ -60,80 +170,99 @@ class AirodumpNg(ExecutorHelper):
            --band <abg>          : Band on which airodump-ng should hop
            -C <frequencies>      : Uses these frequencies in MHz to hop
            --cswitch <method>    : Set channel switching method
-                         0       : FIFO (default)
-                         1       : Round Robin
-                         2       : Hop on last
            -s                    : same as --cswitch
-
            --help                : Displays this usage screen
     """
     requires_tempfile = False
     requires_tempdir = True
     command = "airodump-ng"
 
+    def __init__(self, *args, **kwargs):
+        self.called = False
+        self.proc = None
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        self.run_args = args, kwargs
+        return self
+
+    def __aiter__(self):
+        """Defines us as an async iterator."""
+        return self
+
+    async def __anext__(self):
+        """Get the next result batch."""
+        if not self.called:
+            self.called = True
+            self.proc = await self.run(*self.run_args[0], **self.run_args[1])
+
+        if not self.running:
+            raise StopAsyncIteration
+
+        return await self.results
+
     async def run(self, *args, **kwargs):
         """Run async, with prefix stablished as tempdir."""
+        self.execn += 1
+        kwargs['background'] = 1
         if not ('write' in kwargs or 'w' in kwargs):
             kwargs.pop('w', None)
             kwargs['write'] = self.tempdir.name + "/" + self.uuid
-        asyncio.create_task(self.result_updater())
+        if not 'write_interval' in kwargs:
+            kwargs['write_interval'] = 1
+
+        # Ensure kismet xml is going to be written
+        if kwargs.get('write-format', None) is not None:
+            kwargs['write-format'] = 'kismet,csv,logcsv'
+        elif kwargs.get('write-format') and 'kismet' not in kwargs.get(
+                'write-format', ''):
+            kwargs['write-format'] += ',kismet'
+
         return await super().run(*args, **kwargs)
 
-    async def result_updater(self):
-        """Set result on local object."""
-        while not self.proc:
-            await asyncio.sleep(1)
-        while self.proc.returncode is None:
-            self.meta['result'] = self.get_results()
-            await asyncio.sleep(2)
+    @property
+    def running(self):
+        return self.proc.returncode is None
+
+    def get_file(self, format) -> str:
+        """Return csv file, not kismet one.
+
+        Arguments: 
+
+            format: File extension to retrieve (kismet.csv kismet.xml csv
+                    log.csv or pcap)
+
+        Returns: full filename
+        """
+        return f"{self.tempdir.name}/{self.uuid}-{self.execn:02}.{format}"
 
     @property
-    def csv_file(self):
-        """Return csv file, not kismet one."""
-        glb = glob(self.tempdir.name + "/" + self.uuid + "-*.csv")
-        with suppress(StopIteration):
-            return next(a for a in glb if 'kismet' not in a)
+    async def results(self) -> list:
+        """Return a list of currently detected access points
+        
+        Returns: List of AccessPoint instances
+        """
+        file = self.get_file('kismet.netxml')
+        try:
+            async with timeout(3):
+                while not os.path.exists(file):
+                    await asyncio.sleep(1)
 
-    def get_results(self):
-        """Return results at this moment."""
+                while not self.proc:
+                    await asyncio.sleep(1)
+        except asyncio.exceptions.TimeoutError:
+            raise Exception(await self.proc.communicate())
 
-        if not self.csv_file:
-            return {'clients': [], 'aps': []}
+        while self.running:
+            # Update results each second.
+            with suppress(ValueError, xml.parsers.expat.ExpatError):
+                xmla = xmltodict.parse(open(file).read())
+                dotmap_data = dotmap.DotMap(xmla)
+                results = dotmap_data['detection-run']['wireless-network']
+                if results:
+                    return Result(sorted([AccessPoint(ap) for ap in results],
+                                  key=lambda x: x.score,
+                                  reverse=True))
+                return []
 
-        def clean(line):
-            """Format name."""
-            for name, value in line.items():
-                res = name.strip().lower().replace(' ', '_')
-                yield res.replace('#_', '').replace('-', '_'), value.strip()
-
-        def get_data(content):
-            """Read using a temporary csv file."""
-            with io.StringIO() as fio:
-                fio.write(content)
-                fio.seek(0)
-                for line in [c for c in csv.DictReader(fio, dialect='excel')]:
-                    yield dict(clean(line))
-
-        def get_clients(apoint, clients):
-            """Get clients for a specific ap."""
-            return [c for c in clients if c.bssid == apoint["bssid"]]
-
-        lines = open(self.csv_file).readlines()
-        inx = lines.index('Station MAC, First time seen, Last time seen,'
-                          ' Power, # packets, BSSID, Probed ESSIDs\n')
-
-        clients = ''.join([a for a in lines[inx:] if a.strip()])
-        clients = [Client(**d) for d in get_data(''.join(clients))]
-        aps = ''.join([a for a in lines[:inx] if a.strip()])
-        aps = [
-            AccessPoint(**d, clients=get_clients(d, clients))
-            for d in get_data(''.join(aps))
-        ]
-
-        return {'aps': aps, 'clients': clients}
-
-    def sorted_aps(self):
-        """Return sorted aps by score."""
-        return sorted(self.meta['result']['aps'],
-                      key=lambda x: x.score,
-                      reverse=True)
+            await asyncio.sleep(1)
