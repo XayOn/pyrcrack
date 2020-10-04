05a68b2c0d0bd2fe5d0984f9df842d2e27ed56d6
David Francos <opensource@davidfrancos.net>
2020-10-04 16:11:43 +0200

    closes: #16 Fixed tests
    Lowered test coverage for the moment.

--- docs/examples/scan_for_targets.py
+++ docs/examples/scan_for_targets.py
@@ -1,6 +1,5 @@
 """Scan for targets and and pretty print some data."""
 import asyncio
-import subprocess
 import sys
 
 import pyrcrack
@@ -14,13 +13,14 @@ async def scan_for_targets():
     console.clear()
     console.show_cursor(False)
     async with pyrcrack.AirodumpNg() as pdump:
-        async for result in pdump(sys.argv[1]): 
+        async for result in pdump(sys.argv[1]):
             # Current running process will be stored in self.proc
-            # Be careful, the process will only start when you iter trough results.
-            # a simple await anext(pdump(...)) would do if you don't really
-            # want to gather results.
+            # Be careful, the process will only start when you iter trough
+            # results. A simple await anext(pdump(...)) would do if you don't
+            # reallywant to gather results.
             console.clear()
             console.print(result.table)
             await asyncio.sleep(2)
+
 
 asyncio.run(scan_for_targets())
