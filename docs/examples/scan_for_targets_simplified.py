70412d9838bc458384e5b6be753f2864199a68dc
David Francos <opensource@davidfrancos.net>
2020-10-08 23:36:54 +0200

    Added new example using Pyrcrack simplified api

---
+++ docs/examples/scan_for_targets_simplified.py
@@ -0,0 +1,26 @@
+"""Scan for targets and and pretty print some data."""
+import asyncio
+import sys
+
+import pyrcrack
+
+from rich.console import Console
+from rich.prompt import Prompt
+
+
+async def scan_for_targets():
+    """Scan for targets, return json."""
+    console = Console()
+    console.clear()
+    console.show_cursor(False)
+    pyrc = pyrcrack.Pyrcrack()
+    interface = Prompt.ask('Select an interface',
+                           choices=await pyrc.interfaces)
+
+    await pyrc.set_interface(interface)
+    aps = await pyrc.access_points
+    console.print(aps.table)
+    
+
+
+asyncio.run(scan_for_targets())
