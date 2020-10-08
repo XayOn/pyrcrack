1382c7deb78cb27d9a75986c09cfe0d20f11ad4b
David Francos <opensource@davidfrancos.net>
2020-10-09 00:32:56 +0200

    Improved airmon-ng API to look like airodump's

--- pyrcrack/airmon.py
+++ pyrcrack/airmon.py
@@ -17,6 +17,10 @@ class AirmonNg(ExecutorHelper):
     requires_tempfile = False
     requires_tempdir = False
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.monitor_enabled = []
+
     async def run(self, *args, **kwargs):
         """Check argument position. Forced for this one."""
         if args:
@@ -32,30 +36,63 @@ class AirmonNg(ExecutorHelper):
             reader = csv.DictReader(fileo, dialect='excel-tab')
             return [{a.lower(): b for a, b in row.items()} for row in reader]
 
+    def __call__(self, interface):
+        self.interface = interface
+        return self
+
+    async def __aenter__(self):
+        """Put selected interface in monitor mode."""
+        if not self.interface:
+            raise RuntimeError('Should be called (airmon()) first.')
+        self.interface_data = await self.set_monitor(self.interface)
+        return self
+
+    @property
+    def monitor_interface(self):
+        return self.interface_data[self.interface]['monitor']['interface']
+
+    async def __aexit__(self, *args, **kwargs):
+        """Set monitor-enabled interfaces back to normal"""
+        for interface in self.monitor_enabled:
+            await self.stop(interface)
+
+    async def stop(self, wifi, *args):
+        """Stop monitor mode"""
+        await self.run('stop', wifi, *args)
+
     async def set_monitor(self, wifi, *args):
         """Set monitor mode interface"""
+        assert any(a.get('interface') == wifi
+                   for a in await self.interfaces), 'Wrong interface selected'
+
         await self.run('start', wifi, *args)
         res = [a for a in (await self.proc.communicate())[0].split(b'\n') if a]
         pos = res.index(b'PHY	Interface	Driver		Chipset')
         ifaces_data = self.parse(b'\n'.join(
             [a for a in res[pos:] if a and not a.startswith(b'\t\t')]))
         ifaces = {k['interface']: k for k in ifaces_data}
-        keys = [
-            'driver', 'mode', 'status', 'original_interface',
-            'interface'
-        ]
+        keys = ['driver', 'mode', 'status', 'original_interface', 'interface']
         monitor_lines = res[pos + len(ifaces_data) + 1:]
-        monitor_data = (
-            dict(zip(keys,
-                     MONITOR_RE.match(a.decode()).groups()))
-            for a in monitor_lines if MONITOR_RE.match(a.decode())
-        )
+        monitor_data = (dict(zip(keys,
+                                 MONITOR_RE.match(a.decode()).groups()))
+                        for a in monitor_lines if MONITOR_RE.match(a.decode()))
         for data in monitor_data:
             ifaces[data['original_interface']][data['mode']] = data
+            if data['mode'] == 'monitor':
+                self.monitor_enabled.append(data['interface'])
 
-        return ifaces_data
+        return ifaces
 
-    async def list_wifis(self):
-        """Return a list of wireless networks as advertised by airmon-zc"""
+    @property
+    async def interfaces(self):
+        """List of currently available interfaces as reported by airmon-ng
+
+        This is an awaitable property, use it as in::
+
+        async with AirmonNg() as airmon:
+            await airmon.interfaces
+
+        Returns: None
+        """
         await self.run()
         return self.parse((await self.proc.communicate())[0])
